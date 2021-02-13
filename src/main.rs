7e12f7f60242ee51b5d786a60025be3e10513c67
rpy <rappy.contact@gmail.com>
2021-02-13 16:00:45 +0900

    response index.html

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
 use std::net::TcpListener;
+use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established!");
+
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let mut file = File::open("html/index.html").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
