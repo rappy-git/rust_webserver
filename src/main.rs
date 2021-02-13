db173f8cca258654c62d134cbd07f311ec7daa2e
rpy <rappy.contact@gmail.com>
2021-02-13 16:16:10 +0900

    add 404 page

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,20 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let mut file = File::open("html/index.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let mut file = File::open("html/".to_string() + filename).unwrap();
     let mut contents = String::new();
+
     file.read_to_string(&mut contents).unwrap();
 
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
